895f9cafa47c6118970f951380bb712daa348dde
jaime <jamoncayop@unal.edu.co>
2020-04-02 01:16:19 -0500

    Commit Proyecto presentable

--- motor/MOV_MOTORES/MOV_MOTORES.ino
+++ motor/MOV_MOTORES/MOV_MOTORES.ino
@@ -1,11 +1,12 @@
-#define PIN_MOTOR_1_EN 6
-#define PIN_MOTOR_1_N1 7
-#define PIN_MOTOR_1_N2 5
+
+#define PIN_MOTOR_1_EN 9
+#define PIN_MOTOR_1_N1 10
+#define PIN_MOTOR_1_N2 11
 #define PIN_MOTOR_2_EN 3
 #define PIN_MOTOR_2_N3 4
 #define PIN_MOTOR_2_N4 2
 
-#define MOTOR_1_STOP digitalWrite(PIN_MOTOR_1_N1,LOW);digitalWrite(PIN_MOTOR_1_N2,LOW)
+#define MOTOR_1_STOP digitalWrite(PIN_MOTOR_1_EN, LOW);digitalWrite(PIN_MOTOR_1_N1,LOW);digitalWrite(PIN_MOTOR_1_N2,LOW)
 #define MOTOR_1_FORWARD digitalWrite(PIN_MOTOR_1_N1,LOW);digitalWrite(PIN_MOTOR_1_N2,HIGH)
 #define MOTOR_1_BACKWARD digitalWrite(PIN_MOTOR_1_N1,HIGH);digitalWrite(PIN_MOTOR_1_N2,LOW)
 
@@ -36,8 +37,14 @@ void setup() {
 }
 
 void loop() {
- MOTOR_1_BACKWARD; //ADELANTE
- MOTOR_2_BACKWARD; //ADELANTE
+// MOTOR_1_BACKWARD; //abrir BANDEJA
+// delay(500);
+// MOTOR_1_STOP;
+ MOTOR_1_FORWARD; //abrir BANDEJA
+ delay(500);
+ MOTOR_1_STOP;
+//ADELANTE BANDA EXPULSADA
+// MOTOR_2_BACKWARD; //ADELANTE
 // delay(450);
 // MOTOR_1_FORWARD;  //ATRAS
 // MOTOR_2_FORWARD;  //ATRAS
